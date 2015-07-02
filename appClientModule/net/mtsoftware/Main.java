1c8e03ec41a31a78ac72c55b2c960313cbf456f6
Marek Tutaj <mtutaj@mcw.edu>
2015-07-02 08:58:09 -0500

    day001: show a message box

---
+++ appClientModule/net/mtsoftware/Main.java
@@ -0,0 +1,17 @@
+package net.mtsoftware;
+import javax.swing.JOptionPane;
+
+
+public class Main {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "This is Handmade Hero");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
