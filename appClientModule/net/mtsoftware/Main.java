635d246ecef8db1c5d7f602471b9b45874efea73
Marek Tutaj <marek.tutaj@gmail.com>
2015-07-02 12:09:37 -0500

    day002: show a window (switching between drawing BLACKNESS and WHITENESS)

--- appClientModule/net/mtsoftware/Main.java
+++ appClientModule/net/mtsoftware/Main.java
@@ -1,10 +1,29 @@
 package net.mtsoftware;
-import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 
 public class Main {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "This is Handmade Hero");
+		
+		// create the main frame window
+		JFrame frame = new JFrame("Handmade Hero");
+		
+		// center the main frame window
+		frame.setLocationRelativeTo(null);
+
+		// set initial window size to be 300x100
+		frame.setSize(300, 100);
+		
+		// set custom content pane
+		frame.setContentPane(new MyComponent());
+		
+		// show it
+		frame.setVisible(true);
 	}
 
 	/* (non-Java-doc)
@@ -13,5 +32,23 @@ public class Main {
 	public Main() {
 		super();
 	}
-
+	
+    static class MyComponent extends JComponent {
+    	
+    	static Color color = Color.white;
+    	
+    	public void paint(Graphics g) {
+    		
+    		// alternate between blackness and whiteness
+    		if( color==Color.white ) {
+    			color = Color.black;
+    		} else {
+    			color = Color.white;
+    		}
+    		
+    		// fill the component contents with the chosen color
+    		g.setColor(color);
+    		g.fillRect(0, 0, getWidth(), getHeight());
+	    }
+	}
 }
