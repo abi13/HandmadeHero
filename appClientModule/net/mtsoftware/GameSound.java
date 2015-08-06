8a28b7fd838ddd22ac702fae966880a2ce0a9615
Marek Tutaj <marek.tutaj@gmail.com>
2015-08-06 10:12:13 -0500

    day009: fixed generation of sine wave sound

--- appClientModule/net/mtsoftware/GameSound.java
+++ appClientModule/net/mtsoftware/GameSound.java
@@ -10,10 +10,12 @@ public class GameSound {
 	final int SAMPLE_RATE = 48000;
 	final int SAMPLE_SIZE_IN_BITS = 16;
 	final int CHANNELS = 2;
+	final int BYTES_PER_SAMPLE = CHANNELS * SAMPLE_SIZE_IN_BITS / 8;
 	final boolean DATA_IS_SIGNED = true;
 	final boolean DATA_IS_BIGENDIAN = true;
 	final int TONE_HZ = 261; // middle c tone
 	final int TONE_VOLUME = 100; // 0..127
+	final int WAVE_PERIOD = SAMPLE_RATE / TONE_HZ;
 	
 	SourceDataLine soundLine;
 	long samplesPlayed;
@@ -27,7 +29,7 @@ public class GameSound {
 	        );
         
 		// buffer size in bytes, to hold 1 sec of sound data
-		int bufferSizeInBytes = CHANNELS*SAMPLE_RATE*SAMPLE_SIZE_IN_BITS/8;
+		int bufferSizeInBytes = SAMPLE_RATE * BYTES_PER_SAMPLE;
 		soundLine = (SourceDataLine) AudioSystem.getSourceDataLine(af);
         soundLine.open(af, bufferSizeInBytes);
         soundLine.start();
@@ -42,21 +44,24 @@ public class GameSound {
 		// the buffer size in frames is equal to SAMPLE_RATE (1 sec worth of sound data)
 		samplesPlayed = soundLine.getLongFramePosition();
 		int samplesToWrite = (int) (samplesPlayed + SAMPLE_RATE - samplesWritten);
-		play(samplesToWrite);
-		System.out.println("sframes to write "+samplesToWrite);
+		if( samplesToWrite>0 ) {
+			play(samplesToWrite);
+		}
 	}
 	
 	void play(int samplesToWrite) {
-		int bytesToWrite = samplesToWrite * CHANNELS * SAMPLE_SIZE_IN_BITS/8;
+		
+		int bytesToWrite = samplesToWrite * BYTES_PER_SAMPLE;
 		byte[] buf = new byte[bytesToWrite];
-		int samplingInterval = (int)(CHANNELS * SAMPLE_RATE / TONE_HZ);
-		long sample = samplesWritten;
-		for(int i=0; i<buf.length; i++) {
-			double angle = (2.0 * Math.PI * sample) / samplingInterval;
-			buf[i] = (byte) (Math.sin(angle) * TONE_VOLUME);
-			sample++;
+		for(int i=0; i<buf.length; ) {
+			double angle = (2.0 * Math.PI * samplesWritten) / WAVE_PERIOD;
+			byte value = (byte) (Math.sin(angle) * TONE_VOLUME); 
+			buf[i++] = value;  
+			buf[i++] = value;  
+			buf[i++] = value;  
+			buf[i++] = value;  
+			samplesWritten ++;
 		}
-		samplesWritten += samplesToWrite;
 		soundLine.write(buf, 0, buf.length);
 	}
 }
